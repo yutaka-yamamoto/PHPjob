f793c7a97408a4821add3052a0b52cb53f8a8091
yutaka-yamamoto <100887097+yutaka-yamamoto@users.noreply.github.com>
2022-03-11 13:37:49 +0900

    Initial commit

---
+++ 3-1/3-1.php
@@ -0,0 +1,19 @@
+<?php
+$num = 1;
+while($num < 101) {
+    if ($num %3 == 0 && $num %5 ==0){
+        echo "Fizz!Bizz!"; 
+        echo "<br>";
+    } elseif ($num %5 == 0){
+        echo "Bizz!";
+        echo "<br>";
+    } elseif ($num %3 == 0){
+        echo "Fizz!";
+        echo "<br>";
+    } else {
+        echo $num;
+        echo "<br>";
+        } 
+        $num++;
+}
+?>
